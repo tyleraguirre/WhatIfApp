0fb63b22100a20752735f36a62d151d61dd967a4
tyleraguirre <taguirre6807@xmail.dixietech.edu>
2023-09-11 16:16:26 -0600

    finished mock function for database

--- s&s/viewmodels/FragrenceViewModel.swift
+++ s&s/viewmodels/FragrenceViewModel.swift
@@ -9,13 +9,18 @@ import Foundation
 
 class FragrenceViewModel: ObservableObject {
     
-    @Published var allFragrences: [Fragrence] = [Fragrence(brand: "Versace", scentName: "Good good", color: "Blue", price: 189.00, size: 10), Fragrence(brand: "Hugo Boss", scentName: "Work", color: "Black", price: 40.99, size: 4), Fragrence(brand: "Dolce & Gabana", scentName: "Sweet", color: "Brown", price: 149.00, size: 12)]
+    init() {
+        createDataBase()
+    }
+    
+    @Published var allFragrences: [Fragrence] = []
+    
+    func createDataBase() {
+        allFragrences = [Fragrence(brand: "Versace", scentName: "Good good", color: "Blue", price: 189.00, size: 10), Fragrence(brand: "Hugo Boss", scentName: "Work", color: "Black", price: 40.99, size: 4), Fragrence(brand: "Dolce & Gabana", scentName: "Sweet", color: "Brown", price: 149.00, size: 12), Fragrence(brand: "Ford", scentName: "Mustang", color: "Red", price: 32.99, size: 16)]
+    }
     
     func dispense(fragrence: Fragrence) {
         print("\(fragrence.scentName) has been dispensed")
     }
     
-//    func fragrenceDataBase() {
-//
-//    }
 }
